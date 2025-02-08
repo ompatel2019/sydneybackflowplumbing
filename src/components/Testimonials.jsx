8aa4efc387f57257c67b57c7456680233f32679b
Om Patel <ompatel2019@gmail.com>
2025-02-08 14:30:36 +1100

    testimonials change

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -19,7 +19,7 @@ const Testimonials = () => {
   return (
     <>
       <section className='responsivePad bg-white-0 2xl:py-32 lg:py-24 md:py-20 py-12 text-black-0 space-y-12 max-md:space-y-4 p'>
-        <div className='space-y-4 text-center'>
+        <div className='space-y-4 text-center max-md:space-y-2'>
           <h2 className='h2 font-satoshi-black'>
             {sectionHeading}
           </h2>
@@ -28,9 +28,9 @@ const Testimonials = () => {
           </p>
         </div>
 
-        <div className="flex justify-between max-md:flex-col md:space-x-10 font-satoshi h8">
+        <div className="flex justify-between max-md:flex-col md:space-x-10 font-satoshi h8 max-md:space-y-8">
           {testimonials.map((testimonial, testimonialIndex) => (
-            <div key={testimonialIndex} className="w-full space-y-4 flex flex-col justify-between">
+            <div key={testimonialIndex} className="w-full space-y-4 max-md:space-y-2 flex flex-col justify-between">
               <div className='space-y-4'>
                 <div className="flex text-yellow-400">
                   <FaStar aria-hidden="true" />
